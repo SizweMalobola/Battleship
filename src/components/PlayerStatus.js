c0692545444b8e222a30e1d55545af4c914916df
SizweMalobola <25reyals@gmail.com>
2021-05-14 14:19:32 +0200

    added styling on status panel components

--- src/components/PlayerStatus.js
+++ src/components/PlayerStatus.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { shipsArray } from "../logic";
-import style from "../playerStatus.module.css";
+import styles from "../playerStatus.module.css";
 
 export default class PlayerStatus extends Component {
   constructor(props) {
@@ -13,14 +12,14 @@ export default class PlayerStatus extends Component {
     const { title, isGameOver,player,dimension,changeDimension,shipsPositioned,turn } = this.props;
     return (
       <div>
-        <h1 className={style.title}>{title}</h1>
+        <h1 className={styles.title}>{title}</h1>
         {!isGameOver && (
-          player.playerName === "Human"? <div className={style.prompt}>
-            {shipsPositioned !== 5?<p>position your ships</p>:<p>Attack Computer's Board</p>}
-            {shipsPositioned !==5?<div><button onClick={()=>{changeDimension()}}>Dimension : {dimension} </button><p>ships left to place: {5 - shipsPositioned}</p></div>:<div><p>{turn === "Human"? "your turn": "waiting.."}</p><p>Ships Remaining: {5 - player.playerBoard.sunkShips.length}</p></div>}</div>
-          :<div className={style.prompt}>
-            {shipsPositioned !== 5? <p>waiting...</p>:<p>click on this board to sink their ships</p>}
-            {shipsPositioned !== 5?<div>waiting for you to position your ships</div>:<div><p>{turn === "Human"? "your turn": "waiting.."}</p><p>Ships Remaining: {5 - player.playerBoard.sunkShips.length}</p></div>}
+          player.playerName === "Human"? <div className={styles.panel}>
+            {shipsPositioned !== 5?<p className={styles.prompt}>position your ships</p>:<p className={styles.prompt}>Attack Computer's Board</p>}
+            {shipsPositioned !==5?<div><button className={styles.btn} onClick={()=>{changeDimension()}}>Dimension : {dimension} </button><p className={styles.pill} >ships left to place: {5 - shipsPositioned}</p></div>:<div><p className={styles["turn-pill"]}>{turn === "Human"? "your turn": "waiting.."}</p><p className={styles.pill} >Ships Remaining: {5 - player.playerBoard.sunkShips.length}</p></div>}</div>
+          :<div className={styles.panel}>
+            {shipsPositioned !== 5? <p className={styles.prompt} >waiting...</p>:<p className={styles.prompt} >click on this board to sink their ships</p>}
+            {shipsPositioned !== 5?<div>waiting for you to position your ships</div>:<div><p className={styles["turn-pill"]} >{turn === "Human"? "your turn": "waiting.."}</p><p className={styles.pill}>Ships Remaining: {5 - player.playerBoard.sunkShips.length}</p></div>}
           </div>
         )}
       </div>
