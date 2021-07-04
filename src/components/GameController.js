ac35b499c1e64c508706ebe7b3cc6bb933f60f42
SizweMalobola <25reyals@gmail.com>
2021-07-04 14:07:37 +0200

    refactored clickHandler & compsTurn

--- src/components/GameController.js
+++ src/components/GameController.js
@@ -58,7 +58,6 @@ export default class GameController extends Component {
   playAgain() {
     this.setState({});
   }
-  //!   I don't trust this function but is seems to work for now
   changeTurn() {
     let currentTurn = this.state.turn === "Human" ? "Computer" : "Human";
     this.setState((state) => {
@@ -98,61 +97,56 @@ export default class GameController extends Component {
     }
     this.setState({ computer: computerStateClone });
   }
-  //   anything that has to do with updating state will be done here
+  //   when block on computer board is clicked this function is run and computer board receives an attack.
   clickHandler(target, index) {
-    const player = this.state.computer;
-    // TODO make sure this only runs when player is human
-    // run only if all ships are positioned
+    const computerStateClone = Object.assign({}, this.state.computer);
+    // method short-circuits if ships from human player are positioned
     if (this.state.shipsPositioned !== 5) {
       return;
     }
-    // onclikck board being clicked receives attack
+    // method short-circuits if chosen coordinates to attack has already been attacked previously
     if (
-      player.playerBoard.misses.includes(index) ||
-      player.playerBoard.hits.includes(index)
+      computerStateClone.playerBoard.misses.includes(index) ||
+      computerStateClone.playerBoard.hits.includes(index)
     ) {
       return console.log(
         `You've already picked these coordinates homes. Try different ones.`
       );
     }
     let coordinates = target.parentElement.id;
-    this.setState({
-      player: player.playerBoard.receiveAttack(coordinates),
-    });
+    computerStateClone.playerBoard.receiveAttack(coordinates);
+    this.setState({ computer: computerStateClone });
     // after a hit check wether all the ships are sunk on computer
-    if (this.isGameOver(player)) {
+    if (this.isGameOver(this.state.computer)) {
       this.setState({ gameOver: true });
       console.log("game over Human guy wins");
     } else {
+      // if human player doesn't win round, computer makes its turn.
       // disable computers buttons to prevent click while waiting for computer to make a move
       let boardContainer = target.parentElement.parentElement;
       let btnArray = boardContainer.querySelectorAll("button");
       this.disableButtons(btnArray);
-      // run function that will let computer take a turn
-
       this.changeTurn();
       setTimeout(() => {
         this.compsTurn(this.state.human);
         this.enableButtons(btnArray);
-      }, 1000);
+      }, 700);
     }
   }
 
-  //TODO make this run independently
   compsTurn(player) {
+    const humanStateClone = Object.assign({}, player);
     let target = Math.round(Math.random() * 99);
     while (
-      player.playerBoard.misses.includes(target) ||
-      player.playerBoard.hits.includes(target)
+      humanStateClone.playerBoard.misses.includes(target) ||
+      humanStateClone.playerBoard.hits.includes(target)
     ) {
       target = Math.round(Math.random() * 99);
     }
-    let coordinates = player.playerBoard.board[target];
-    this.setState({
-      player: player.playerBoard.receiveAttack(coordinates),
-    });
-    //
-    if (this.isGameOver(player)) {
+    let coordinates = humanStateClone.playerBoard.board[target];
+    humanStateClone.playerBoard.receiveAttack(coordinates);
+    this.setState({ human: humanStateClone });
+    if (this.isGameOver(this.state.human)) {
       this.setState({ gameOver: true });
     } else {
       this.changeTurn();
